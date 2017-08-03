cb4aea9fe574971285c75742737287f41a03bea2
Jamal Carter <JamalC77@users.noreply.github.com>
2017-08-03 15:22:56 -0500

    First Commit

---
+++ FlappyBird/GameOverViewController.swift
@@ -0,0 +1,51 @@
+//
+//  GameOverViewController.swift
+//  FlappyBird
+//
+//  Created by Jamal Carter on 4/14/17.
+//  Copyright © 2017 Fullstack.io. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+@available(iOS 9.0, *)
+class GameOverViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let scene = GameScene.unarchiveFromFile("GameScene") as? GameScene {
+            // Configure the view.
+            let skView = self.view as! SKView
+            skView.showsFPS = false
+            skView.showsNodeCount = false
+            
+            /* Sprite Kit applies additional optimizations to improve rendering performance */
+            skView.ignoresSiblingOrder = true
+            
+            /* Set the scale mode to scale to fit the window */
+            scene.scaleMode = .aspectFill
+            
+            skView.presentScene(scene)
+        }
+    }
+    
+    override var shouldAutorotate : Bool {
+        return true
+    }
+    
+    override var supportedInterfaceOrientations : UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return UIInterfaceOrientationMask.allButUpsideDown
+        } else {
+            return UIInterfaceOrientationMask.all
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+    
+}
